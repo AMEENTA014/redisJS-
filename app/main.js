924117b65f6b8cd4a95f27a12e208a15eb4643f6
AMEENTA014 <ameenta41@gmail.com>
2025-10-18 23:20:02 +0530

    created a server in js

--- app/main.js
+++ app/main.js
@@ -1,11 +1,17 @@
-const net = require("net");
+//exports from net module is needed in this program 
+//else how can we use the export and create a socket 
+const net = require("net");   
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+
+//console is an object over the abstraction 
+//process.stdout and std err 
+// which is abstracted or the fd which is needed to the ipc to out the information 
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-// const server = net.createServer((connection) => {
-//   // Handle connection
-// });
-//
-// server.listen(6379, "127.0.0.1");
+//net is still needed to access 
+//and net provides a method to create a socket fd 
+// then we assign the object to the server 
+ const server = net.createServer((connection) => {
+});
+//listen inorder to create a queue under the kernel
+server.listen(6379, "127.0.0.1");
