013e2bc39edccbc8b8d2a4b6621e747a4ba9164f
AMEENTA014 <ameenta41@gmail.com>
2025-10-19 14:23:59 +0530

    managed multiple pongs

--- app/main.js
+++ app/main.js
@@ -8,11 +8,15 @@ const net = require("net");
 // which is abstracted or the fd which is needed to the ipc to out the information 
 console.log("Logs from your program will appear here!");
 
-//net is still needed to access 
-//and net provides a method to create a socket fd 
-// then we assign the object to the server 
- const server = net.createServer((connection) => {//net have a net.Socket defined which returns a socket 
-    connection._write('+PONG\r\n');
+
+ const server = net.createServer((connection) => {//create server creates the socket fd 
+    //and we need socket fd to access inorder to read and write to the network interface 
+ connection.on('data',(data)=>{ //so after creating this method allows to pass a callback function 
+       console.log(data);      //when ever the socket is created the callback is executed with the socket or server object is passed 
+       connection._write('+PONG\r\n');//with we just need to create an event with the server object 
+       //when a connection is called on the server object event listener is called which says PONG 
+ })  
+ 
 });
 //listen inorder to create a queue under the kernel
 server.listen(6379, "127.0.0.1");
