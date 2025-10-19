4bcaad2be81febb1b6ae18966cce7de1f0c8bfc2
AMEENTA014 <ameenta41@gmail.com>
2025-10-19 13:52:29 +0530

    send response pong

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 //exports from net module is needed in this program 
 //else how can we use the export and create a socket 
 const net = require("net");   
-
+//net allows to create socket under tcp : hey i want to connect : lets connect if ur serious and send me connect : connect back 
 
 //console is an object over the abstraction 
 //process.stdout and std err 
@@ -11,7 +11,11 @@ console.log("Logs from your program will appear here!");
 //net is still needed to access 
 //and net provides a method to create a socket fd 
 // then we assign the object to the server 
- const server = net.createServer((connection) => {
+ const server = net.createServer((connection) => {//net have a net.Socket defined which returns a socket 
+    connection._write('+PONG\r\n');
 });
 //listen inorder to create a queue under the kernel
 server.listen(6379, "127.0.0.1");
+
+//whats an even listener array 
+//why the on method stores the callback reference to the listener array 
